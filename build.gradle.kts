659bf15ebeb98b85ed79cee8afc6833de315aef2
Cristina Zoccola <72309137+criicchio@users.noreply.github.com>
2026-04-26 12:28:20 +0200

    Added junit tests dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,7 @@ repositories {
 }
 dependencies {
     implementation("org.scala-lang:scala3-library_3:3.3.7")
+    testImplementation(platform("org.junit:junit-bom:6.0.3"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
